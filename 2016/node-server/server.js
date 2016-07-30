bd8e1edb251c0322ed4e5dd95a01d1e3609a4bb2
James Hagerman <james.hagerman@gmail.com>
2016-07-29 22:29:59 -0700

    Updating the server.js to the CORRECT version. Adding html5 boilerplate to get us a starting point for the server.

--- 2016/node-server/server.js
+++ 2016/node-server/server.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
 
+// Share the public directory:
+app.use(express.static('public'));
+
 app.get('/currentAnimation/:id', function (req, res) {
 	var currentAnimation = req.id;
-	switchAnimation(currentAnimation);
-  	res.send('Switching animation!');
+	// switchAnimation(currentAnimation);
+  	res.status(200).send('Switching animation to: ', currentAnimation);
 });
+
+
+app.get('/xyPad', function (req, res) {
+  res.send('xyPad World!');
+});
+
+
+app.get('/color', function (req, res) {
+  res.send('color World!');
+});
+
+
+app.get('/accel', function (req, res) {
+  res.send('accel World!');
+});
+
+
+app.get('/sound', function (req, res) {
+  res.send('sound World!');
+});
+
+
+app.get('/gps', function (req, res) {
+  res.send('gps World!');
+});
+
+
+app.get('/camera', function (req, res) {
+  res.send('camera World!');
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
