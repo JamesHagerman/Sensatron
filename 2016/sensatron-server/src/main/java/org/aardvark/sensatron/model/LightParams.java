f528dcbd977652d8d27dcfbdf302f42baaf60ac1
Jacob Finn <super.aardvark@gmail.com>
2016-08-18 20:18:35 -0700

    Add UI elements to LightParams model

--- 2016/sensatron-server/src/main/java/org/aardvark/sensatron/model/LightParams.java
+++ 2016/sensatron-server/src/main/java/org/aardvark/sensatron/model/LightParams.java
@@ -1,8 +1,15 @@
 package org.aardvark.sensatron.model;
 
+import java.util.Arrays;
+
 public class LightParams implements Cloneable {
 	
 	private boolean on;
+	private int hue1;
+	private int hue2;
+	private int brightness;
+	private int slider4;
+	private int[] pitchSliders = new int[12];
 
 	public boolean isOn() {
 		return on;
@@ -14,11 +21,52 @@ public class LightParams implements Cloneable {
 
 	@Override
 	public String toString() {
-		return "LightParams [on=" + on + "]";
+		return "LightParams [on=" + on + ", hue1=" + hue1 + ", hue2=" + hue2 + ", brightness=" + brightness
+				+ ", slider4=" + slider4 + ", pitchSliders=" + Arrays.toString(pitchSliders) + "]";
 	}
 
 	@Override
 	public LightParams clone() throws CloneNotSupportedException {
 		return (LightParams) super.clone();
+	}
+
+	public int getHue1() {
+		return hue1;
+	}
+
+	public void setHue1(int hue1) {
+		this.hue1 = hue1;
+	}
+
+	public int getHue2() {
+		return hue2;
+	}
+
+	public void setHue2(int hue2) {
+		this.hue2 = hue2;
+	}
+
+	public int getBrightness() {
+		return brightness;
+	}
+
+	public void setBrightness(int brightness) {
+		this.brightness = brightness;
+	}
+
+	public int getSlider4() {
+		return slider4;
+	}
+
+	public void setSlider4(int slider4) {
+		this.slider4 = slider4;
+	}
+
+	public int[] getPitchSliders() {
+		return pitchSliders;
+	}
+
+	public void setPitchSliders(int[] pitchSliders) {
+		this.pitchSliders = pitchSliders;
 	}
 }
